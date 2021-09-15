cf768bf6315788117732790fa84c2c7ce3ae1235
Daniel Vega <danielalexvega@gmail.com>
2021-09-15 00:37:03 -0500

    finish solving functions

--- src/Components/CalculatorFeatures/CalculatorContainer.js
+++ src/Components/CalculatorFeatures/CalculatorContainer.js
@@ -11,6 +11,65 @@ import OPERATION_ARR from '../SharedStatic/OPERATION_ARR';
 
 const CalculatorContainer = () => {
     const [display, setDisplay] = useState('');
+    const [containsError, setContainsError] = useState(false);
+
+    function removeDoubleNegatives(valueArr) {
+        for (let i = 0; i < valueArr.length - 1; i++) {
+            if (valueArr[i] === "-" && valueArr[i + 1] === "-") {
+                valueArr[i] = "+";
+                valueArr.splice(i + 1, 1);
+            }
+        }
+
+        return valueArr;
+    }
+
+    function leadingNegative(valueArr) {
+        if (valueArr.length > 1 && valueArr[0] === "-" && valueArr[1] !== "-") {
+            let updatedValue = "-" + valueArr[1]
+            valueArr.splice(0, 2, updatedValue);
+        }
+
+        console.log(valueArr);
+        return valueArr;
+    }
+
+    function plusMinus(valueArr) {
+        for (let i = 0; i < valueArr.length - 1; i++) {
+            if (valueArr[i] === "+" && valueArr[i + 1] === "-") {
+                valueArr[i] = "-";
+                valueArr.splice(i + 1, 1);
+            }
+        }
+
+        return valueArr;
+    }
+
+    function containsParenthesis(valueArr) {
+        let validEquation = false;
+        let containsFrontParenthesis = false;
+        for (let i = 0; i < valueArr.length - 1; i++) {
+            if (valueArr[i] === ")" && !containsFrontParenthesis) {
+                displayError();
+                return;
+            }
+
+            if (valueArr[i] === "(") {
+                containsFrontParenthesis = true;
+                for (let j = i + 1; j < valueArr.length; j++) {
+                    if (valueArr[j] === ")") {
+                        validEquation = true;
+                    }
+                }
+                if (!validEquation) {
+                    displayError();
+                    return;
+                }
+            }
+        }
+
+        return validEquation;
+    }
 
 
     function findOperations(valueArr) {
@@ -18,7 +77,6 @@ const CalculatorContainer = () => {
         modifiedOperationsArr.push("(");
         modifiedOperationsArr.push(")");
         let opertionsArr = [];
-        console.log(modifiedOperationsArr);
         valueArr.forEach((value, index) => {
             if (modifiedOperationsArr.includes(value)) {
                 opertionsArr.push(index);
@@ -32,8 +90,6 @@ const CalculatorContainer = () => {
         let integerArr = [];
         let operationArr = [];
         let tempInt = "";
-        // console.log(valueArr);
-        // console.log(valueArr.length);
         for (let i = 0; i < valueArr.length; i++) {
             if (!operationsIndexArr.includes(i) && !operationsIndexArr.includes(i + 1)) {
                 tempInt += valueArr[i];
@@ -50,16 +106,153 @@ const CalculatorContainer = () => {
         return { integerArr, operationArr };
     }
 
-    function calculuate(valueArr) {
-        let operationsIndexArr = findOperations(valueArr);
-        let { integerArr, operationArr } = findIntegerOperationArr(valueArr, operationsIndexArr);
+    function displayError() {
+        setContainsError(true);
+        setDisplay("Error");
+    }
 
-        console.log(integerArr);  // integer array
-        console.log(operationArr);  //operation array
+    function simplifyEquation(equation) {
 
-        // Need to check for double negatives
-        // need to check for parenthesis
-        // do the math
+        let frontParenthesis = equation.findIndex(value => value === "(");
+        let closingParenthesis = equation.lastIndexOf(")");
+
+        if (closingParenthesis === -1) {
+            displayError();
+        }
+
+        let insideEquation = equation.filter((integer, index) => (index > frontParenthesis && index < closingParenthesis));
+        if (containsParenthesis(insideEquation)) {
+            do {
+                insideEquation = simplifyEquation(insideEquation);
+
+            } while (containsParenthesis(insideEquation))
+        }
+
+        let solution = solveSetup(insideEquation);
+
+        equation.splice(frontParenthesis, (closingParenthesis - frontParenthesis + 1), solution);
+        console.log(equation);
+        return equation;
+    }
+
+    function solveSetup(equation) {
+        let operationsIndexArr = findOperations(equation);
+        let integerArr = [];
+        let operationArr = [];
+        let tempInt = "";
+        for (let i = 0; i < equation.length; i++) {
+            if (!operationsIndexArr.includes(i) && !operationsIndexArr.includes(i + 1)) {
+                tempInt += equation[i];
+            } else if (!operationsIndexArr.includes(i)) {
+                tempInt += equation[i];
+                integerArr.push(parseInt(tempInt, 10));
+                tempInt = "";
+            } else {
+                operationArr.push(equation[i])
+            }
+        }
+        integerArr.push(parseInt(tempInt, 10));
+
+        console.log(integerArr);
+        console.log(operationArr);
+
+        let solution = solve(integerArr, operationArr);
+        return solution;
+    }
+
+    function solve(integerArr, operationArr) {
+        let first_order = ["*", "/", "%"];
+        let second_order = ["+", "-"];
+
+        console.log("before everything");
+        console.log(integerArr);
+        console.log(operationArr);
+
+        //first order operations
+        for (let i = 0; i < integerArr.length - 1; i++) {
+            if (first_order.includes(operationArr[i])) {
+                let tempSolution = operate(operationArr[i], integerArr[i], integerArr[i + 1]);
+
+                operationArr.splice(i, 1);
+                integerArr.splice(i, 2, tempSolution)
+                i--;
+
+                console.log(`We got here`);
+                console.log(operationArr);
+                console.log(integerArr);
+            }
+        }
+        if (integerArr.length === 1) {
+            return integerArr[0];
+        }
+
+        let solution = 0;
+
+        for (let i = 0; i < integerArr.length - 1; i++) {
+            if (second_order.includes(operationArr[i]) && solution === 0) {
+                solution = operate(operationArr[i], integerArr[i], integerArr[i + 1]);
+            } else {
+                solution = operate(operationArr[i], solution, integerArr[i + 1]);
+            }
+        }
+
+        console.log(`the solution is ${solution}`);
+
+        return solution;
+    }
+
+    function operate(operation, value1, value2) {
+        let solution;
+        switch (operation) {
+            case "+":
+                solution = value1 + value2;
+                break;
+            case "-":
+                solution = value1 - value2;
+                break;
+            case "*":
+                solution = value1 * value2;
+                break;
+            case "/":
+                solution = value1 / value2;
+                break;
+            case "%":
+                solution = value1 % value2;
+                break;
+            default:
+        }
+        return solution;
+    }
+
+    function calculuate(display) {
+        let equation = display.split("");
+
+        if (OPERATION_ARR.includes(equation[equation.length - 1])) {
+            displayError();
+            return;
+        }
+
+        equation = removeDoubleNegatives(equation);
+        equation = leadingNegative(equation);
+        equation = plusMinus(equation);
+
+        //check for parenthesis
+        if (containsParenthesis(equation)) {
+            do {
+                equation = simplifyEquation(equation);
+
+            } while (containsParenthesis(equation))
+            //if there are double parenthesis, then we have to preform those operations first
+        }
+
+        //at this point there are no parenthesis
+
+
+        let operationsIndexArr = findOperations(equation);
+        let { integerArr, operationArr } = findIntegerOperationArr(equation, operationsIndexArr);
+        let solution = solve(integerArr, operationArr);
+
+        console.log(`This is the final solution: ${solution}`);
 
     }
 
@@ -68,12 +261,11 @@ const CalculatorContainer = () => {
         switch (event.target.value) {
             case 'AC':
                 setDisplay('');
+                setContainsError(false);
                 break;
             case '=':
-                calculuate(display.split(""));
+                calculuate(display);
                 break;
-
-
             default:
                 setDisplay(() => display + event.target.value);
                 break;
@@ -90,6 +282,7 @@ const CalculatorContainer = () => {
                 <IntegerKeypad handleButtonClick={handleIntegerClick} />
                 <OperationContainer handleButtonClick={handleIntegerClick} />
             </div>
+            {containsError && <div>ERROR</div>}
         </div>
     )
 }
